cdcd7a2d5b86029a217bd10bfba8d1cbc7da33b0
njcushing <129661299+njcushing@users.noreply.github.com>
2023-05-01 12:35:36 +0100

    Add password-matching function and update form input styling accordingly

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("hello World!")
+var passwordPrimaryInput = document.querySelector(".form-content-inputs-container input#password-primary");
+var passwordSecondaryInput = document.querySelector(".form-content-inputs-container input#password-secondary");
+passwordPrimaryInput.addEventListener('input', passwordChanged);
+passwordSecondaryInput.addEventListener('input', passwordChanged);
+
+function passwordChanged(e) {
+
+    if(!passwordPrimaryInput.validity.patternMismatch &&
+        passwordSecondaryInput.value !== passwordPrimaryInput.value &&
+        passwordPrimaryInput.value !== ''
+    ){
+        passwordPrimaryInput.classList.add("error");
+    } else {
+        passwordPrimaryInput.classList.remove("error");
+    }
+
+    if(!passwordSecondaryInput.validity.patternMismatch &&
+        passwordSecondaryInput.value !== passwordPrimaryInput.value &&
+        passwordSecondaryInput.value !== ''
+    ){
+        passwordSecondaryInput.classList.add("error");
+    } else {
+        passwordSecondaryInput.classList.remove("error");
+    }
+
+
+}
